4ac17c1bbf3cc4e5883ea9e6042411502951d592
Lucien <luiskeniosis@gmail.com>
2020-06-25 10:55:30 -0400

    Version 1.01
    Already found a bug. Ooop. Fixed the issue, you'll no longer be able to
    run the /conjoin command if either Movement or Interaction controller is
    not assigned.

--- Project/src/lucien/ConjoinedMinecraft/commands/ConjoinCommand.java
+++ Project/src/lucien/ConjoinedMinecraft/commands/ConjoinCommand.java
@@ -14,18 +14,24 @@ public class ConjoinCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] arguments) {
 	if(arguments.length == 0) {
-	    Main.isConjoined = true;
-	    for(Player player : Bukkit.getOnlinePlayers()) {
-		player.setBedSpawnLocation(player.getLocation(), true);
-		player.setHealth(20.0);
-		player.setFoodLevel(20);
-		player.setCollidable(false);
+	    if(Main.movementController != null && Main.interactionController != null) {
+		Main.isConjoined = true;
+		for(Player player : Bukkit.getOnlinePlayers()) {
+		    player.setBedSpawnLocation(player.getLocation(), true);
+		    player.setHealth(20.0);
+		    player.setFoodLevel(20);
+		    player.setCollidable(false);
+		}
+		Player interactController = Bukkit.getPlayer(Main.interactionController);
+		Player movementController = Bukkit.getPlayer(Main.movementController);
+		interactController.hidePlayer(Main.plugin, movementController);
+		movementController.hidePlayer(Main.plugin, interactController);
+		HealthAndHungerSyncHandlers.syncHunger();
 	    }
-	    Player interactController = Bukkit.getPlayer(Main.interactionController);
-	    Player movementController = Bukkit.getPlayer(Main.movementController);
-	    interactController.hidePlayer(Main.plugin, movementController);
-	    movementController.hidePlayer(Main.plugin, interactController);
-	    HealthAndHungerSyncHandlers.syncHunger();
+	    else {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&5Lucien&l&dAI&r&8] &cBoth Move and Interact controllers must be assigned before conjoining!"));
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&5Lucien&l&dAI&r&8] &f/movement <player_name> &eand &f/interact <player_name> &eto assign controllers."));
+	    }
 	}
 	else
 	    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&5Lucien&l&dAI&r&8] &cInvalid arguments!"));
